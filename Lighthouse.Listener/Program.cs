f46653b77d87213c2d3ba2205ca47a0bdf05326b
Matt Booth <matt-booth@live.co.uk>
2024-08-10 20:55:46 +0100

    Setup Program.cs

--- Lighthouse.Listener/Program.cs
+++ Lighthouse.Listener/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Net.WebSockets;
+using System.Text;
 
-Console.WriteLine("Hello, World!");
+namespace Lighthouse.Listener
+{
+  internal class Program
+  {
+    public static async Task Main(string[] args)
+    {
+      CancellationTokenSource source = new CancellationTokenSource();
+      CancellationToken token = source.Token;
+      using (var ws = new ClientWebSocket())
+      {
+        await ws.ConnectAsync(new Uri("wss://stream.aisstream.io/v0/stream"), token);
+        await ws.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes("{ \"APIKey\": \"<YOUR API KEY>\", \"BoundingBoxes\": [[[-11.0, 178.0], [30.0, 74.0]]]}")), WebSocketMessageType.Text, true, token);
+        byte[] buffer = new byte[4096];
+        while (ws.State == WebSocketState.Open)
+        {
+          var result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), token);
+          if (result.MessageType == WebSocketMessageType.Close)
+          {
+            await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, token);
+          }
+          else
+          {
+            Console.WriteLine($"Received {Encoding.Default.GetString(buffer, 0, result.Count)}");
+          }
+        }
+      }
+    }
+  }
+}
