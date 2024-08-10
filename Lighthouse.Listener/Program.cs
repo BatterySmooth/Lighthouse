8c30a3f94feaea07c7836da7d7bfe264c296dc3e
Matt Booth <matt-booth@live.co.uk>
2024-08-10 22:32:01 +0100

    Added Listener logic

--- Lighthouse.Listener/Program.cs
+++ Lighthouse.Listener/Program.cs
@@ -1,32 +1,64 @@
-﻿using System.Net.WebSockets;
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net.WebSockets;
 using System.Text;
+using Lighthouse.Listener.Messages;
+using Microsoft.CSharp.RuntimeBinder;
 
 namespace Lighthouse.Listener
 {
   internal class Program
   {
+    public static IConfiguration? Configuration;
+    
     public static async Task Main(string[] args)
     {
+      // Get secrets config
+      Configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("secrets.json", optional: true, reloadOnChange: true)
+        .Build();
+      
+      var connectionMsg = new ConnectionMessage
+      {
+        APIKey = Configuration["AISKey"] ?? throw new Exception("No AIS API Key has been supplied"),
+        BoundingBoxes = new List<List<List<double>>> { new() { new List<double> {-180, -90}, new List<double> {180, 90} } },
+        FiltersShipMMSI = new List<string> { "235018216", "311045300" },
+        FilterMessageTypes = new List<string> { "PositionReport" }
+      };
+      
+      string jsonString = JsonConvert.SerializeObject(connectionMsg);
+      
+      Console.WriteLine(jsonString);
+      
       CancellationTokenSource source = new CancellationTokenSource();
       CancellationToken token = source.Token;
-      using (var ws = new ClientWebSocket())
+      
+      using var ws = new ClientWebSocket();
+      await ws.ConnectAsync(new Uri("wss://stream.aisstream.io/v0/stream"), token);
+      await ws.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(jsonString)), WebSocketMessageType.Text, true, token);
+      byte[] buffer = new byte[4096];
+      while (ws.State == WebSocketState.Open)
       {
-        await ws.ConnectAsync(new Uri("wss://stream.aisstream.io/v0/stream"), token);
-        await ws.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes("{ \"APIKey\": \"<YOUR API KEY>\", \"BoundingBoxes\": [[[-11.0, 178.0], [30.0, 74.0]]]}")), WebSocketMessageType.Text, true, token);
-        byte[] buffer = new byte[4096];
-        while (ws.State == WebSocketState.Open)
+        var result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), token);
+        if (result.MessageType == WebSocketMessageType.Close)
         {
-          var result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), token);
-          if (result.MessageType == WebSocketMessageType.Close)
-          {
-            await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, token);
-          }
-          else
-          {
-            Console.WriteLine($"Received {Encoding.Default.GetString(buffer, 0, result.Count)}");
-          }
+          Console.WriteLine("Websocket closed. Attempting to reconnect...");
+          await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, token);
+        }
+        else
+        {
+          Console.WriteLine($"Received {Encoding.Default.GetString(buffer, 0, result.Count)}");
         }
       }
+    }
+    
+    private static async Task DelayWithExponentialBackOff(int attempt)
+    {
+      int delay = Math.Min(2000 * (int)Math.Pow(2, attempt), 30000); // Start at 2 seconds, double each time up to 30 seconds
+      Console.WriteLine($"Waiting {delay} ms before retrying...");
+      await Task.Delay(delay);
     }
   }
 }
